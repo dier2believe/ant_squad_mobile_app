ed3fb35542102569e9cdac35cdf970a716ccd610
dier2believe <dier2believe@gmail.com>
2017-03-02 23:07:07 -0700

    Create houseBuilding.js

---
+++ building/houseBuilding.js
@@ -0,0 +1,21 @@
+/*global answerButton1*/
+/*global answerButton2*/
+/*global output*/
+/*global players*/
+/*global activePlayer*/
+/*global playerMenuDisplay*/
+/*global randNum*/
+/*global house*/
+
+//         HOUSE BUILDING
+
+function houseBuilding() {
+    /*
+    output.innerHTML = "Would you like to store something at home?";
+    
+    */
+    
+    output.innerHTML = "You look up in pride at your home, then continue on your way.";
+    
+    playerMenuDisplay();
+}
