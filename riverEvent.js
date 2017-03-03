f7c46cc5310ca0258e8fca9790fbcc00bbe8b836
dier2believe <dier2believe@gmail.com>
2017-03-02 23:18:53 -0700

    Update riverEvent.js

--- riverEvent.js
+++ riverEvent.js
@@ -1,1 +1,19 @@
+/*global answerButton1*/
+/*global answerButton2*/
+/*global output*/
+/*global players*/
+/*global activePlayer*/
+/*global playerMenuDisplay*/
+/*global randNum*/
+/*global river*/
 
+//         RIVER EVENTS
+
+//  river.randEvents = [];
+
+function riverEvent() {
+    
+    output.innerHTML = "You wade through the water.";
+    
+    playerMenuDisplay();
+}
