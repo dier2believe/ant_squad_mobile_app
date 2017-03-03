2e5739ec407ca22bfaf910bd6a1073280a001e36
dier2believe <dier2believe@gmail.com>
2017-03-02 23:13:53 -0700

    Update cemetaryEvent.js

--- cemetaryEvent.js
+++ cemetaryEvent.js
@@ -1,1 +1,19 @@
+/*global answerButton1*/
+/*global answerButton2*/
+/*global output*/
+/*global players*/
+/*global activePlayer*/
+/*global playerMenuDisplay*/
+/*global randNum*/
+/*global cemetary*/
 
+//         CEMETARY EVENTS
+
+//  cemetary.randEvents = [];
+
+function cemetaryEvent() {
+    
+    output.innerHTML = "You feel a chill in the air as you by the sullen graves.";
+    
+    playerMenuDisplay();
+}
