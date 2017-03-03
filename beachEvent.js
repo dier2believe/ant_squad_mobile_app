71fbe5dfe3638abf9a04d23e7dd0d3c3def9a34d
dier2believe <dier2believe@gmail.com>
2017-03-02 23:13:12 -0700

    Update beachEvent.js

--- beachEvent.js
+++ beachEvent.js
@@ -1,1 +1,19 @@
+/*global answerButton1*/
+/*global answerButton2*/
+/*global output*/
+/*global players*/
+/*global activePlayer*/
+/*global playerMenuDisplay*/
+/*global randNum*/
+/*global beach*/
 
+//         BEACH EVENTS
+
+//  beach.randEvents = [];
+
+function beachEvent() {
+    
+    output.innerHTML = "You happily walk along the beach";
+    
+    playerMenuDisplay();
+}
