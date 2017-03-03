05d25b892b4c1cc875f27a42ff10fe61149da7a0
dier2believe <dier2believe@gmail.com>
2017-03-02 23:10:28 -0700

    Create storeBuilding.js

---
+++ buildings/storeBuilding.js
@@ -0,0 +1,16 @@
+/*global answerButton1*/
+/*global answerButton2*/
+/*global output*/
+/*global players*/
+/*global activePlayer*/
+/*global playerMenuDisplay*/
+/*global randNum*/
+/*global store*/
+
+//         STORE BUILDING
+
+function storeBuilding() {
+    output.innerHTML = "Store<br>Closed for renovations.";
+    
+    playerMenuDisplay();
+}
