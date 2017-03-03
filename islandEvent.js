592415e7398b3b2802e1957c857c42eb4095b35f
dier2believe <dier2believe@gmail.com>
2017-03-02 23:17:14 -0700

    Update islandEvent.js

--- islandEvent.js
+++ islandEvent.js
@@ -1,1 +1,19 @@
+/*global answerButton1*/
+/*global answerButton2*/
+/*global output*/
+/*global players*/
+/*global activePlayer*/
+/*global playerMenuDisplay*/
+/*global randNum*/
+/*global island*/
 
+//         ISLAND EVENTS
+
+//  island.randEvents = [];
+
+function islandEvent() {
+    
+    output.innerHTML = "You feel the sand under your feet as you walk across the island.";
+    
+    playerMenuDisplay();
+}
